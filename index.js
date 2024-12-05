0125b08c50413599033d95ac465f186b751ce47c
Security testing <118594933+w2h3@users.noreply.github.com>
2024-12-05 17:53:18 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+
+const port = process.env.PORT || 5006
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('pages/index')
+})
+
+const server = app.listen(port, () => {
+  console.log(`Listening on ${port}`)
+})
+
+process.on('SIGTERM', async () => {
+  console.log('SIGTERM signal received: gracefully shutting down')
+  if (server) {
+    server.close(() => {
+      console.log('HTTP server closed')
+    })
+  }
+})
